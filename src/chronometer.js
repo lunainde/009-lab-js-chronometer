ec0e8c2d6564978ee9f1fd17fb0c85a3126c4332
lunainde <lunainde@gmail.com>
2024-02-03 16:39:31 +0100

    lab 009 Chronometer done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    // currentTime, with the initial value set to 0.
+    this.currentTime = 0;
+    this.intervalId = null;
+
+    // intervalId, with the initial value set to null.
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    // should increment by 1 the currentTime property every 1 second
+    this.intervalId = setInterval(() => {
+            this.currentTime += 1;
+    // should invoke the passed printTimeCallback every 1 second
+            if(printTimeCallback){
+              printTimeCallback(this.currentTime);
+            }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    // let count = this.currentTime /60
+    // let minuteCount = Math.floor(count)
+    // return minuteCount
+
+    return Math.floor(this.currentTime /60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    
+    return this.currentTime % 60 
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+
+    let result = value.toString()
+    if(result.length < 2){
+      result = "0" + result.slice(0,1)
+    }
+    return result
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+   
+    let secondeSplit = this.getSeconds().toString()
+    let minuteSplit = this.getMinutes().toString()
+
+
+  if(secondeSplit.length < 2){
+    secondeSplit = "0" + secondeSplit.slice(0,1)
+    }
+    if(minuteSplit.length < 2){
+      minuteSplit = "0" + minuteSplit.slice(0,1)
+    }
+
+    return `${minuteSplit}:${secondeSplit}`
+
   }
 }
